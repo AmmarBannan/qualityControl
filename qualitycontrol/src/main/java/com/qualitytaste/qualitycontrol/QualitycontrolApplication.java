0dff67ba2eb7cb3a7b8409482197d75249bdd8ae
ammar bannan <ammarabannan@gmail.com>
2024-11-09 21:15:29 +0200

    pc success

---
+++ qualitycontrol/src/main/java/com/qualitytaste/qualitycontrol/QualitycontrolApplication.java
@@ -0,0 +1,13 @@
+package com.qualitytaste.qualitycontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QualitycontrolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QualitycontrolApplication.class, args);
+	}
+
+}
